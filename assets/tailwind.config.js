29e30d9f6f6d99430bcf8d7bfe237b6b01207fba
eduarchilon <eduarchilon@outlook.es>
2023-03-16 15:29:48 -0300

    feat(cart oage): create cart page and quantity selector

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -9,6 +9,17 @@ tailwind.config = {
       'white': '#ffff',
       'secondary-white': '#f2f2f2',
       'third-grey': '#dee2e6',
+      'primary-red': "red",
+      'primary-green': "#88c8bc",
+      grey: {
+        50: '#f0f0f0',
+        100: '#616161',
+        150: '#88c8bc',
+        200: 'rgba(0, 0, 0, 0.3)',
+        250: '#ccc6c6',
+        300:'#dee2e6',
+        400:'#f0ecec',
+      },
     },
   },
 }
