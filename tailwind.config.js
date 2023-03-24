769542b2ab2cfeaf62310c2c76b230fc50fab114
eduarchilon <eduarchilon@outlook.es>
2023-03-23 23:46:05 -0300

    feat(variant-selector): create filter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 tailwind.config = {
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
+  ],
   theme: {
     extend: {},
     container: {
@@ -6,19 +16,19 @@ tailwind.config = {
     },
     colors: {
       'primary-brown': '#616161',
-      'white': '#ffff',
+      white: '#ffff',
       'secondary-white': '#f2f2f2',
       'third-grey': '#dee2e6',
-      'primary-red': "red",
-      'primary-green': "#88c8bc",
+      'primary-red': 'red',
+      'primary-green': '#88c8bc',
       grey: {
         50: '#f0f0f0',
         100: '#616161',
         150: '#88c8bc',
         200: 'rgba(0, 0, 0, 0.3)',
         250: '#ccc6c6',
-        300:'#dee2e6',
-        400:'#f0ecec',
+        300: '#dee2e6',
+        400: '#f0ecec',
       },
     },
   },
