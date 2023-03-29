b79e9014b8bc382a188d6ccba75330035c0bddcd
eduarchilon <eduarchilon@outlook.es>
2023-03-29 15:18:12 -0300

    feat(search): create search forms

---
+++ assets/search.js
@@ -0,0 +1,7 @@
+let formHeaderSearch = document.getElementById('search-header')
+let valueSearchPage = document.getElementById('input-search-page')
+let headerSearch = document.getElementById('input-search-header')
+
+if (valueSearchPage.value !== '') {
+  headerSearch.value = ''
+}
